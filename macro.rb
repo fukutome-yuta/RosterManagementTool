f7d4fa48b84545044c87e284d6dcdffd31913783
fukutome-yuta <44062419+fukutome-yuta@users.noreply.github.com>
2019-03-07 17:32:47 +0900

    Update macro.rb

--- macro.rb
+++ macro.rb
@@ -23,27 +23,31 @@ end
 
 def greeting(today)
 
-  now = today.strftime('%Y年 %m月 %x日 (%a)')
+  now = today.strftime('%Y年 %m月 %d日 (%a)')
   judge_hour = today.hour
 
-  if judge_hour >= 8 and judge_hour <= 12　then
-    greeting = 'おはよう！\n今日は ' + now
-  elsif judge_hour >=13 and judge_hour <= 16 then
-    greeting = 'こんにちは！\n今日は ' + now
-  elsif judge_hour == 17 then
-    greeting = 'お疲れ様！\n今日は ' + now
+  if judge_hour >= 8 and judge_hour <= 12
+    @greeting = "おはよう！\n今日は " + now
+    @closing_remarks = '今日も一日頑張ろう！'
+  elsif judge_hour >=13 and judge_hour <= 16
+    @greeting = "こんにちは！\n今日は " + now
+    @closing_remarks = 'あと少し！頑張ってね！'
+  elsif judge_hour == 17
+    @greeting = "お疲れ様！\n今日は " + now
+    @closing_remarks = '今日も一日お疲れ様！'
   end
 
-  puts greeting
+  puts @greeting
+
 end
 
 def send_mail(excel, file)
 
   Mail.defaults do
     delivery_method :smtp, {
-      :address => 'smtp.gmail.com',
-      :port => 587,
-      :domain => 'example.com',
+      :address => 'sample',
+      :port => 25,
+      :domain => 'sample',
       :user_name => "#{mail_from}",
       :password => "#{mail_passwd}",
       :authentication => :login,
@@ -52,80 +56,97 @@ def send_mail(excel, file)
   end
 
   mail = Mail.new do
-    from    'from@example.co.jp'
-    to      'to@example.co.jp'
+    from    'sample'
+    to      'sample'
     subject 'subject text'
-    body    ''
+    body     File.read("./body.txt")
     add_file './sample/xlxs'
   end
   
 end
 
+def update_clock_out(clock_out)
+
+    tmp_clock_out_cell = cell.Address.to_s
+    clock_out_cell = sheet.range(tmp_clock_out_cell.gsub(/A/, 'D'))
+    target_cell_No = tmp_clock_out_cell.delete("^0-9").to_i
+
+    while clock_out_cell.value == nil do      
+      target_cell_No = target_cell_No - 1
+      target_cell_address = '$D$' + target_cell_No.to_s
+      clock_out_cell = sheet.range(target_cell_address)
+    end
+
+    clock_out_cell.value = clock_out
+
+end
+
+def update_clock_in(clock_in)
+
+  tmp_clock_in_cell = cell.Address.to_s
+  clock_in_cell = sheet.range(tmp_clock_in_cell.gsub(/A/, 'C'))
+  clock_in_cell.value = clock_in
+
+end
 
 def update_excel(excel, file, sheet_num = 1, today)
-  
-  puts '直近の退勤時間を入力してね！'
-  tmp_end_time = gets
-  
-  puts '今日の出勤時間を入力してね！'
-  tmp_start_time = gets
-  
-  go_home_time = Time.parse(tmp_end_time)
-  attendance_time = Time.parse(tmp_start_time)
-  
+
   book = excel.Workbooks.Open(file)
   sheet = book.Worksheets(sheet_num)
+
+  loop do
+    puts '出退勤時刻を更新する？(y/n)'
+    @answer_of_update = gets.chomp
+
+    if @answer_of_update != "y" and @answer_of_update != "n"
+      puts '「y」か「n」で入力してね'
+    elsif  @answer_of_update == "y" or @answer_of_update == "n"
+      break
+    end
+    
+  end
+  
+  if @answer_of_update == 'y'
+    puts '直近の退勤時間を入力してね！'
+    tmp_clock_out = gets
+    
+    puts '今日の出勤時間を入力してね！'
+    tmp_clock_in = gets
+    
+    clock_out = Time.parse(tmp_clock_out)
+    clock_in = Time.parse(tmp_clock_in)
+  end
   
   last_day = today.day - 1
 
-    sheet.range('A10:A40').each do |cell|
+  sheet.range('A10:A40').each do |cell|
 
-      tmp_day = cell.value
+    tmp_day = cell.value
 
-      if last_day == tmp_day.day then
-        
-        tmp_end_cell = cell.Address.to_s
-        go_home_cell = sheet.range(tmp_end_cell.gsub(/A/, 'D'))
-        target_cell_No = tmp_end_cell.delete("^0-9").to_i
-
-        while go_home_cell.value == nil do
-          
-          target_cell_No = target_cell_No - 1
-          target_cell_address = '$D$' + target_cell_No.to_s
-          go_home_cell = sheet.range(target_cell_address)
-
-        end
-
-        go_home_cell.value = go_home_time
-
+    if @answer_of_update == 'y'
+      if last_day == tmp_day.day
+        update_clock_out(clock_out)
       end
 
-      if today.day == tmp_day.day then
-
-        tmp_start_cell = cell.Address.to_s
-        attendance_cell = sheet.range(tmp_start_cell.gsub(/A/, 'C'))
-        attendance_cell.value = attendance_time
-
+      if today.day == tmp_day.day
+        update_clock_in(clock_in)
       end
-      
-    end
-        
-    # 保存
-    book.saveAs(file)
-
-    # ファイルを閉じる
-    book.close
-        
-    puts '更新完了！'
-
-  end  
+      puts '更新完了！'
+    end 
+  end
+  # 保存
+  book.saveAs(file)
+  # ファイルを閉じる
+  book.close
+end
 
 def main()
 
   today = Time.now()
 
   greeting(today)
-  
+
+
   # OLE32用FileSystemObject生成
   fso = WIN32OLE.new('Scripting.FileSystemObject')
   #file = fso.GetAbsolutePathName('./sample.xlsx')
@@ -133,13 +154,15 @@ def main()
 
   excel = init_excel()
 
-  send_mail(excel, file)
+  #send_mail(excel, file)
   update_excel(excel, file, today)
 
   excel.quit()
+
+  puts @closing_remarks
+  sleep(3)
+
 end
 
 main()
 
-puts 'END'
-sleep(3)
