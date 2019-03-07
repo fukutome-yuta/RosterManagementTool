e76bed360e1d609bc2433f82409ac925cb345a23
fukutome-yuta <a.zero.yu@gmail.com>
2019-03-08 07:04:17 +0900

    2019/03/08

--- macro.rb
+++ macro.rb
@@ -4,7 +4,8 @@ STDOUT.sync = true
 
 require 'win32ole'
 require 'time'
-require 'mail'
+require 'date'
+#require 'mail'
 
 # Excel VBA定数のロード
 module Excel; end
@@ -22,124 +23,217 @@ def init_excel()
 end
 
 def greeting(today)
-
-  now = today.strftime('%Y年 %m月 %x日 (%a)')
+  now = today.strftime('%Y年 %m月 %d日 (%a)')
   judge_hour = today.hour
 
-  if judge_hour >= 8 and judge_hour <= 12　then
-    greeting = 'おはよう！\n今日は ' + now
-  elsif judge_hour >=13 and judge_hour <= 16 then
-    greeting = 'こんにちは！\n今日は ' + now
-  elsif judge_hour == 17 then
-    greeting = 'お疲れ様！\n今日は ' + now
+  if judge_hour >= 8 and judge_hour <= 12
+    opening_remarks = "おはよう！\n今日は " + now
+    @closing_remarks = '今日も一日頑張ろう！'
+  elsif judge_hour >=13 and judge_hour <= 16
+    opening_remarks = "こんにちは！\n今日は " + now
+    @closing_remarks = 'あと少し！頑張ってね！'
+  elsif judge_hour >= 17
+    opening_remarks = "お疲れ様！\n今日は " + now
+    @closing_remarks = '今日も一日お疲れ様！'
   end
-
-  puts greeting
+  puts opening_remarks
 end
 
-def send_mail(excel, file)
-
-  Mail.defaults do
-    delivery_method :smtp, {
-      :address => 'smtp.gmail.com',
-      :port => 587,
-      :domain => 'example.com',
-      :user_name => "#{mail_from}",
-      :password => "#{mail_passwd}",
-      :authentication => :login,
-      :enable_starttls_auto => true
-    }
+def find_target_cell(cell, purpose)
+  case purpose
+  when 'Update' then
+    purpose = '$D$'
+  when 'SendMail' then
+    purpose = '$A$'
   end
 
-  mail = Mail.new do
-    from    'from@example.co.jp'
-    to      'to@example.co.jp'
-    subject 'subject text'
-    body    ''
-    add_file './sample/xlxs'
+  tmp_target_cell_address = cell.Address.to_s
+  target_cell = sheet.range(tmp_target_cell_address.gsub(/A/, 'D'))
+  target_cell_address_No = tmp_target_cell.delete("^0-9").to_i
+
+  while target_cell.value == nil do      
+    target_cell_address_No = target_cell_address_No - 1
+    target_cell_address = purpose + target_cell_address_No.to_s
+    target_cell = sheet.range(target_cell_address)
   end
-  
+  return target_cell 
 end
 
+def update_clock_in(clock_in)
+  tmp_clock_in_cell = cell.Address.to_s
+  clock_in_cell = sheet.range(tmp_clock_in_cell.gsub(/A/, 'C'))
+  clock_in_cell.value = clock_in
+end
+
+def validate_input(question)
+  loop do
+    puts question
+    answer = gets.chomp
+
+    if answer != "y" and answer != "n"
+      puts '「y」か「n」で入力してね'
+    elsif answer == "y" or answer == "n"
+      return　answer
+      break
+    end
+  end
+end
 
 def update_excel(excel, file, sheet_num = 1, today)
-  
-  puts '直近の退勤時間を入力してね！'
-  tmp_end_time = gets
-  
-  puts '今日の出勤時間を入力してね！'
-  tmp_start_time = gets
-  
-  go_home_time = Time.parse(tmp_end_time)
-  attendance_time = Time.parse(tmp_start_time)
-  
   book = excel.Workbooks.Open(file)
   sheet = book.Worksheets(sheet_num)
+
+  work_time_question = '出退勤時刻を更新する？(y/n)'
+  answer_of_update = validate_input(work_time_question)
+  
+  if answer_of_update == 'y'
+    puts '直近の退勤時間を入力してね！'
+    tmp_clock_out = gets
+    puts '今日の出勤時間を入力してね！'
+    tmp_clock_in = gets
+    
+    clock_out = Time.parse(tmp_clock_out)
+    clock_in = Time.parse(tmp_clock_in)
+  end
   
   last_day = today.day - 1
+  end_of_month = Date.new(today.year, today.month, -1)
 
-    sheet.range('A10:A40').each do |cell|
+  sheet.range('A10:A40').each do |cell|
+    tmp_day = cell.value
 
-      tmp_day = cell.value
-
-      if last_day == tmp_day.day then
-        
-        tmp_end_cell = cell.Address.to_s
-        go_home_cell = sheet.range(tmp_end_cell.gsub(/A/, 'D'))
-        target_cell_No = tmp_end_cell.delete("^0-9").to_i
-
-        while go_home_cell.value == nil do
-          
-          target_cell_No = target_cell_No - 1
-          target_cell_address = '$D$' + target_cell_No.to_s
-          go_home_cell = sheet.range(target_cell_address)
-
-        end
-
-        go_home_cell.value = go_home_time
-
+    if answer_of_update == 'y'
+      if last_day == tmp_day.day
+        purpose = 'Update'
+        clock_out_cell = find_target_cell(cell, purpose)
+        clock_out_cell.value = clock_out
       end
 
-      if today.day == tmp_day.day then
-
-        tmp_start_cell = cell.Address.to_s
-        attendance_cell = sheet.range(tmp_start_cell.gsub(/A/, 'C'))
-        attendance_cell.value = attendance_time
-
+      if today.day == tmp_day.day
+        update_clock_in(clock_in)
       end
-      
+      puts '更新完了！'
     end
-        
-    # 保存
-    book.saveAs(file)
 
-    # ファイルを閉じる
-    book.close
-        
-    puts '更新完了！'
+    case tmp_day.day
+    when 18 then
+      purpose = 'SendMail'
+      @to_bright_day = find_target_cell(cell, purpose)
+    when end_of_month.day then
+      purpose = 'SendMail'
+      @to_me_day = find_target_cell(cell, purpose)
+    end
+  end
+  # 保存
+  book.saveAs(file)
+  # ファイルを閉じる
+  book.close
+end
 
-  end  
+def sendmail_decision(today)
+  case today.day
+  when @to_bright_day.day then
+    puts '今日は現場勤務表提出日だよ！勤務表の中身を確認してね！'
+    question = '今すぐ自社にメールを送る？(y/n)'
+    answer_of_sendmail = validate_input(question)
+    if answer_of_sendmail == 'y'
+      destination = 'bright'
+      send_mail(destination, today)
+    elsif answer_of_sendmail == 'n'
+      puts '今日中に送ってね！'
+    end
+  when @to_me_day.day then
+    puts '今日は月末だよ！勤務表の中身を確認してね！'
+    question = '今すぐ自分宛てにメールを送る？(y/n)'
+    answer_of_sendmail = validate_input(question)
+    if answer_of_sendmail == 'y'
+      destination = 'me'
+      send_mail(destination, today)
+    elsif answer_of_sendmail == 'n'
+      puts '今日中に送ってね！'
+    end
+  end
+end
+
+def send_mail(destination, today)
+
+  mail_info = mail_creation(destination, today)
+
+  puts "メールの内容を確認してね！\n差出人：#{mail_info[from]}\n宛先：#{mail_info[to]}\
+  \ncc：#{mail_info[cc]}\n件名：#{mail_info[subject]}\n本文：#{mail_info[body]}"
+  question = 'この内容で送ってもいい？(y/n)'
+  sendmail_decision = validate_input(question)
+  
+  if sendmail_decision == 'y'
+    Mail.defaults do
+      delivery_method :smtp, {
+        :address => 'sample',
+        :port => 25,
+        :domain => 'sample',
+        :user_name => "#{mail_from}",
+        :password => "#{mail_passwd}",
+        :authentication => :login,
+        :enable_starttls_auto => true
+      }
+    end
+
+    mail = Mail.new do
+      from     "#{mail_info[from]}"
+      to       "#{mail_info[to]}"
+      cc       "#{mail_info[cc]}"
+      subject  "#{mail_info[subject]}"
+      body     "#{mail_info[body]}"
+      add_file ""
+    end
+    mail.deliver
+    puts '送信完了！'
+  elsif sendmail_decision == 'n'
+    puts '送信をキャンセルしたよ！'
+  end
+end
+
+def mail_creation(destination, today)
+
+  subject = today.strftime('_勤務表 %Y年%m月分')
+  case destination
+  when 'bright'
+    mail_info = {
+                  from:     '',
+                  to:       '',
+                  cc:       '',
+                  subject:  '',
+                  body:     "各位\n\nお疲れ様です。です。\n今月分の現場勤務表を送付致します。\nご確認よろしくお願いいたします。\n\n"
+                }
+  when 'me'
+    mail_info = {
+                  from:     '',
+                  to:       '',
+                  cc:       '',
+                  subject:  '',
+                  body:     '今日中に自社勤務表を宛にメールしてね！'
+                }
+  end
+  return mail_info
+end
 
 def main()
-
-  today = Time.now()
+  today = Time.now
 
   greeting(today)
-  
+
   # OLE32用FileSystemObject生成
   fso = WIN32OLE.new('Scripting.FileSystemObject')
   #file = fso.GetAbsolutePathName('./sample.xlsx')
   file = fso.GetAbsolutePathName('sample.xlsm')
 
   excel = init_excel()
-
-  send_mail(excel, file)
   update_excel(excel, file, today)
-
   excel.quit()
+
+  sendmail_decision(today) 
+
+  puts @closing_remarks
+  sleep(3)
 end
 
 main()
-
-puts 'END'
-sleep(3)
